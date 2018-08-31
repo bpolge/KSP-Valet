67c5cc8b9960bd6a670f7901df5891006d9a44ec
Benjamin Polge <ben@teamredhq.com>
2018-08-31 13:01:41 -0500

    Added Line to target

--- MyFirstKSPModule/HelloKerbinMod.cs
+++ MyFirstKSPModule/HelloKerbinMod.cs
@@ -9,9 +9,228 @@ namespace MyFirstKSPModule
 {
     public class HelloKerbinMod : PartModule
     {
+        TargetingRenderer targetLines;
         public override void OnStart(StartState state)
         {
-            Debug.LogError("MY_FIRST: OnStart");
+            Debug.Log("MY_FIRST: OnStart");
+
+            // Make sure there aren't duplicates attached to the vessel
+            vessel.OnFlyByWire -= OnFlyByWire;
+            vessel.OnFlyByWire += OnFlyByWire;
+
+            targetLines = FlightCamera.fetch.mainCamera.gameObject.AddComponent<TargetingRenderer>();
+        }
+
+        private ITargetable targetedVessel;
+        private Vessel activeVessel;
+        private Vector3 relVelocityTarget = new Vector3();
+        private void Update()
+        {
+            targetLines.enabled = false;
+            targetLines.Lines.Clear();
+            if (HighLogic.LoadedSceneIsFlight && !FlightGlobals.ActiveVessel.isEVA && !MapView.MapIsEnabled)
+            {
+                targetedVessel = FlightGlobals.fetch.VesselTarget;
+                activeVessel = FlightGlobals.ActiveVessel;
+
+                if (targetedVessel == null) return;
+
+                relVelocityTarget = targetedVessel.GetObtVelocity() - activeVessel.GetObtVelocity();
+
+                //FlightGlobals.ActiveVessel.ActionGroups.ToggleGroup(KSPActionGroup.RCS);
+                if (relVelocityTarget != null && !relVelocityTarget.IsZero())
+                {
+                    Vector3 v = Quaternion.Inverse(vessel.GetTransform().rotation) * relVelocityTarget;
+                    Vector3 p = activeVessel.transform.position;
+                    Vector3 tP = targetedVessel.GetTransform().position;
+
+                    TargetingLine vesselToTarget = new TargetingLine(p, tP);
+                    targetLines.Add(vesselToTarget);
+
+                    //st.X = clamp((float)v.x, -1, 1);
+                    //st.Y = clamp((float)v.z, -1, 1);
+                    //st.Z = clamp((float)v.y, -1, 1);
+                }
+                targetLines.enabled = true;
+
+
+            }
+        }
+
+        private void OnFlyByWire(FlightCtrlState st)
+        {
+
+        }
+
+        private float clamp(float x, float min, float max)
+        {
+            if (x < min) return min;
+            if (x > max) return max;
+            return x;
+        }
+        
+
+    }
+
+    public class TargetingLine {
+        public List<Vector3> Verticies = new List<Vector3>();
+
+        public Vector3 From {
+            get => Verticies[0];
+            set => Verticies.Insert(0, value);
+        }
+
+        public Vector3 To {
+            get => Verticies[1];
+            set => Verticies.Insert(1, value);
+        }
+
+        public TargetingLine(Vector3 from, Vector3 to)
+        {
+            this.From = from;
+            this.To = to;
         }
     }
+
+    public class TargetingRenderer: MonoBehaviour
+    {
+        public List<TargetingLine> Lines = new List<TargetingLine>();
+
+        static float lineWidth = 2;
+        public float LineWidth {
+            set => lineWidth = value;
+        }
+        private Material material;
+
+        public void Add(TargetingLine line) {
+            Lines.Add(line);
+        }
+
+        public void OnPostRender()
+        {
+            if (Lines.Count == 0) return;
+            //Debug.Log("DRAWING!!!");
+            if (material == null) material = new Material(Shader.Find("Particles/Additive"));
+
+            foreach (TargetingLine line in Lines)
+            {
+                GL.PushMatrix();
+                material.SetPass(0);
+                GL.LoadPixelMatrix();
+                GL.Begin(GL.QUADS);
+                GL.Color(Color.blue);
+
+                Debug.Log("ADDING POINTS");
+                Debug.Log(this.enabled);
+                //for (int i = 0; i < Vertices.Count; i++)
+                //{
+                //    Vector3 p = Vertices[i];
+                //    Vector3 sp = FlightCamera.fetch.mainCamera.WorldToScreenPoint(p);
+                //    Debug.Log(sp);
+                //    GL.Vertex3(p.x, p.z, 0);
+                //}
+                foreach (Vector3 vertex in line.Verticies)
+                {
+                    Vector3 point = FlightCamera.fetch.mainCamera.WorldToScreenPoint(vertex);
+                    GL.Vertex3(point.x, point.y, 0);
+                    GL.Vertex3(point.x + lineWidth, point.y, 0);
+                }
+                GL.End();
+                GL.PopMatrix();
+            }
+        }
+    }
+
+    //public override void Drive(FlightCtrlState s)
+    //{
+    //    setPIDParameters();
+
+    //    switch (controlType)
+    //    {
+    //        case ControlType.TARGET_VELOCITY:
+    //            // Removed the gravity since it also affect the target and we don't know the target pos here.
+    //            // Since the difference is negligable for docking it's removed
+    //            // TODO : add it back once we use the RCS Controler for other use than docking. Account for current acceleration beside gravity ?
+    //            worldVelocityDelta = vesselState.orbitalVelocity - targetVelocity;
+    //            //worldVelocityDelta += TimeWarp.fixedDeltaTime * vesselState.gravityForce; //account for one frame's worth of gravity
+    //            //worldVelocityDelta -= TimeWarp.fixedDeltaTime * gravityForce = FlightGlobals.getGeeForceAtPosition(  Here be the target position  ); ; //account for one frame's worth of gravity
+    //            break;
+
+    //        case ControlType.VELOCITY_ERROR:
+    //            // worldVelocityDelta already contains the velocity error
+    //            break;
+
+    //        case ControlType.VELOCITY_TARGET_REL:
+    //            if (core.target.Target == null)
+    //            {
+    //                rcsDeactivate();
+    //                return;
+    //            }
+
+    //            worldVelocityDelta = core.target.RelativeVelocity - targetVelocity;
+    //            break;
+    //    }
+
+    //    // We work in local vessel coordinate
+    //    Vector3d velocityDelta = Quaternion.Inverse(vessel.GetTransform().rotation) * worldVelocityDelta;
+
+    //    if (!conserveFuel || (velocityDelta.magnitude > conserveThreshold))
+    //    {
+    //        if (!vessel.ActionGroups[KSPActionGroup.RCS])
+    //        {
+    //            vessel.ActionGroups.SetGroup(KSPActionGroup.RCS, true);
+    //        }
+
+    //        Vector3d rcs = new Vector3d();
+
+    //        for (int i = 0; i < Vector6.Values.Length; i++)
+    //        {
+    //            Vector6.Direction dir = Vector6.Values[i];
+    //            double dirDv = Vector3d.Dot(velocityDelta, Vector6.directions[(int)dir]);
+    //            double dirAvail = vesselState.rcsThrustAvailable[dir];
+    //            if (dirAvail > 0 && Math.Abs(dirDv) > 0.001)
+    //            {
+    //                double dirAction = dirDv / (dirAvail * TimeWarp.fixedDeltaTime / vesselState.mass);
+    //                if (dirAction > 0)
+    //                {
+    //                    rcs += Vector6.directions[(int)dir] * dirAction;
+    //                }
+    //            }
+    //        }
+
+    //        Vector3d omega = Vector3d.zero;
+
+    //        switch (controlType)
+    //        {
+    //            case ControlType.TARGET_VELOCITY:
+    //                omega = Quaternion.Inverse(vessel.GetTransform().rotation) * (vessel.acceleration - vesselState.gravityForce);
+    //                break;
+
+    //            case ControlType.VELOCITY_TARGET_REL:
+    //            case ControlType.VELOCITY_ERROR:
+    //                omega = (worldVelocityDelta - prev_worldVelocityDelta) / TimeWarp.fixedDeltaTime;
+    //                prev_worldVelocityDelta = worldVelocityDelta;
+    //                break;
+    //        }
+
+    //        rcs = pid.Compute(rcs, omega);
+
+    //        // Disabled the low pass filter for now. Was doing more harm than good
+    //        //rcs = lastAct + (rcs - lastAct) * (1 / ((Tf / TimeWarp.fixedDeltaTime) + 1));
+    //        lastAct = rcs;
+
+    //        s.X = Mathf.Clamp((float)rcs.x, -1, 1);
+    //        s.Y = Mathf.Clamp((float)rcs.z, -1, 1); //note that z and
+    //        s.Z = Mathf.Clamp((float)rcs.y, -1, 1); //y must be swapped
+    //    }
+    //    else if (conserveFuel)
+    //    {
+    //        if (vessel.ActionGroups[KSPActionGroup.RCS])
+    //        {
+    //            vessel.ActionGroups.SetGroup(KSPActionGroup.RCS, false);
+    //        }
+    //    }
+
+    //    base.Drive(s);
+    //}
 }
--- MyFirstKSPModule/Park.cs
+++ MyFirstKSPModule/Park.cs
@@ -7,7 +7,7 @@ using KSP.UI.Screens;
 
 namespace MyFirstKSPModule
 {
-    [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
     public class Park : MonoBehaviour
     {
         private static ApplicationLauncherButton appLaunchButton;
@@ -22,7 +22,7 @@ namespace MyFirstKSPModule
                     onTrue,
                     onFalse,
                     null, null, null, null,
-                    ApplicationLauncher.AppScenes.SPACECENTER | ApplicationLauncher.AppScenes.FLIGHT,
+                    ApplicationLauncher.AppScenes.FLIGHT,
                     appLauncherIcon
                 );
             }
@@ -30,6 +30,7 @@ namespace MyFirstKSPModule
 
         public void Start()
         {
+
             GameEvents.onGUIApplicationLauncherReady.Add(AddToStockAppLauncher);
             GameEvents.onGUIApplicationLauncherDestroyed.Add(delegate {
                 if (appLaunchButton != null)
@@ -39,54 +40,100 @@ namespace MyFirstKSPModule
             });
         }
 
-        private bool _displayGUI = false;
-        private Rect _windowPostition = new Rect(20f, 100f, 250f, 150f);
+        private bool displayGUI = false;
+        private Rect windowPostition = new Rect(20f, 100f, 250f, 150f);
         private void onShowGUI() {
             Debug.Log("PARK_MOD::DEBUG: onShowGUI");
-            _displayGUI = true;
+            displayGUI = true;
         }
 
+        private void onHideGUI()
+        {
+            Debug.Log("PARK_MOD::DEBUG: onHideGUI");
+            displayGUI = false;
+        }
+
+        private static ITargetable targetedVessel;
+        private static ITargetable activeVessel;
         private void OnWindow(int id)
         {
             GUIStyle label = new GUIStyle(GUI.skin.button);
+            label.fixedWidth = 100f;
 
+            GUILayout.BeginVertical();
             // Debug.Log("PARK_MOD::DEBUG: OnWindow");
-            GUILayout.BeginHorizontal(GUILayout.Width(250f));
-            GUILayout.Label("This is my Layout", label);
-            GUILayout.Label("YESSIR");
-            GUILayout.EndHorizontal();
+            if (targetedVessel != null)
+            {
+                GUILayout.BeginHorizontal(GUILayout.Width(250f));
+                GUILayout.Label("TRG VSL", label);
+                GUILayout.Label(targetedVessel.GetDisplayName());
+                GUILayout.EndHorizontal();
 
-            GUILayout.BeginHorizontal(GUILayout.Width(250f));
-            GUILayout.Label("Another one", label);
-            GUILayout.EndHorizontal();
+                GUILayout.BeginHorizontal(GUILayout.Width(250f));
+                GUILayout.Label("TRG V", label);
+                GUILayout.Label(FlightGlobals.fetch.vesselTargetDelta.ToString());
+                GUILayout.EndHorizontal();
 
+                GUILayout.BeginHorizontal(GUILayout.Width(250f));
+                GUILayout.Label("TRG Orb V", label);
+                GUILayout.Label(targetedVessel.GetObtVelocity().ToString());
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal(GUILayout.Width(250f));
+                GUILayout.Label("ACT Orb V", label);
+                GUILayout.Label(activeVessel.GetObtVelocity().ToString());
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal(GUILayout.Width(250f));
+                GUILayout.Label("REL V", label);
+                GUILayout.Label(relVelocityTarget.ToString());
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal(GUILayout.Width(250f));
+                GUILayout.Label("REL Vm", label);
+                GUILayout.Label(relVelocityTarget.magnitude.ToString());
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal(GUILayout.Width(250f));
+                GUILayout.Label("ACT V", label);
+                GUILayout.Label(activeVessel.GetObtVelocity().ToString());
+                GUILayout.EndHorizontal();
+
+                GUILayout.BeginHorizontal(GUILayout.Width(250f));
+                GUILayout.Label("FWD V", label);
+                GUILayout.Label(activeVessel.GetFwdVector().ToString());
+                GUILayout.EndHorizontal();
+            } else {
+                GUILayout.Label("No Vessel Targeted");
+            }
+            GUILayout.EndVertical();
             GUI.DragWindow();
-        }
-
-        private void onHideGUI() {
-            Debug.Log("PARK_MOD::DEBUG: onHideGUI");
-            _displayGUI = false;
         }
 
         public void OnGUI()
         {
-            // Debug.Log("PARK_MOD::DEBUG: OnGUI");
-            if (_displayGUI)
-                _windowPostition = GUILayout.Window(1800, _windowPostition, OnWindow, "This Is My Title");
+            if (displayGUI)
+                windowPostition = GUILayout.Window(2000, windowPostition, OnWindow, "This Is My Title");
         }
 
         public void Awake()
         {
             byte[] arrBytes;
             arrBytes = KSP.IO.File.ReadAllBytes<Park>("TeamRedLogo.png", null);
-            Debug.Log("!!!!!!!!!!!!!!!");
             Debug.Log(arrBytes.ToString());
             appLauncherIcon.LoadImage(arrBytes);
         }
 
+        private static Vector3 relVelocityTarget = new Vector3();
         public void Update()
         {
-            // Debug.Log("PARK_MOD::DEBUG: Upodate");
+            if (HighLogic.LoadedSceneIsFlight && !FlightGlobals.ActiveVessel.isEVA && !MapView.MapIsEnabled) {
+                targetedVessel = FlightGlobals.fetch.VesselTarget;
+                activeVessel = FlightGlobals.ActiveVessel;
+
+                if (targetedVessel != null)
+                    relVelocityTarget = targetedVessel.GetObtVelocity() - activeVessel.GetObtVelocity();
+            }
         }
 
 
